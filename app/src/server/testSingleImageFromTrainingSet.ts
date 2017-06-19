bc02d98b58f0ace2703d0d35c9e75589adeca05a
Patrick Desjardins <mrdesjardins@gmail.com>
2017-06-19 11:07:17 -0700

    Detect people and identify from  a single testing script

---
+++ app/src/server/testSingleImageFromTrainingSet.ts
@@ -0,0 +1,79 @@
+import * as fs from "fs";
+import * as request from "request";
+
+import { visionEndpointURL, faceEndpointURL, imageGroupId, imageGroupDisplayName } from "../../../config";
+import { computerVisionKey, localDirectory, trainDirectory, faceApiKey } from "../../../secret";
+
+const testImageFullPath = "D:\\code\\CognitiveImagesCollection\\trainimages\\meloAndAlicia.jpg";
+
+
+// Get Emotion of a single image
+function detectPeopleFromImage(imageFullPath: string): Promise<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+        const urlAzure = faceEndpointURL + `/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,smile,facialHair,glasses,emotion,hair,accessories`;
+        console.log(urlAzure);
+        const req = fs.createReadStream(imageFullPath).pipe(request({
+            url: urlAzure,
+            method: "POST",
+            encoding: "binary",
+            headers: {
+                "Content-Type": "application/octet-stream",
+                "Host": "westus.api.cognitive.microsoft.com",
+                "Ocp-Apim-Subscription-Key": faceApiKey
+            }
+        }, (error, response, body) => {
+            if (error) {
+                console.error(error);
+                reject(error);
+            } else {
+                console.log(body);
+                resolve(JSON.parse(body));
+            }
+        }));
+    });
+    return promise;
+}
+
+
+// Identify
+function identifyPeople(personGroupId: string, detectPayload: any): Promise<void> {
+    const promise = new Promise<any>((resolve, reject) => {
+        const arrayIds = (detectPayload as any[]).map((entry) => entry.faceId);
+        console.log(arrayIds);
+        const urlAzure = faceEndpointURL + `/identify`;
+        console.log(urlAzure);
+        request({
+            url: urlAzure,
+            method: "POST",
+            json: {
+                personGroupId: personGroupId,
+                faceIds: arrayIds,
+                maxNumOfCandidatesReturned: 3,
+                confidenceThreshold: 0.5
+            },
+            headers: {
+                "Content-Type": "application/json",
+                "Host": "westus.api.cognitive.microsoft.com",
+                "Ocp-Apim-Subscription-Key": faceApiKey
+            }
+        }, (error, response, body) => {
+            if (error) {
+                console.error(error);
+                reject(error);
+            } else {
+                for (let i = 0; i < body.length; i++) {
+                    console.log(body[i].candidates);
+                }               
+                resolve(body);
+            }
+        });
+    });
+    return promise;
+}
+
+
+detectPeopleFromImage(testImageFullPath)
+    .then((payload: any) => {
+        identifyPeople(imageGroupId, payload);
+    });
+
--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 export const visionEndpointURL = "https://westus.api.cognitive.microsoft.com/vision/v1.0";
 export const faceEndpointURL = "https://westus.api.cognitive.microsoft.com/face/v1.0";
-
 export const imageGroupId = "immediatefamily";
 export const imageGroupDisplayName = "My Immediate Family";
